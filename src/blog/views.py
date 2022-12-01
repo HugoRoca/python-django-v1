39433a3df71fecaa04a868af893745e1b79be2f0
HugoRoca <hugo.rock20@hotmail.com>
2022-12-01 08:51:30 -0500

    Login and bootstrap

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {
         'message': 'From view'
+    })
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user:
+            login(request, user)
+            messages.success(request, 'Welcome {}'.format(user.username))
+            return redirect('index')
+        else:
+            messages.error(request, 'Incorrect username or password.')
+
+    return render(request, 'login.html', {
+
     })
---
+++ src/static/js/functions.js
@@ -0,0 +1,3 @@
+function salu2(){
+    alert("Hello world!")
+}
